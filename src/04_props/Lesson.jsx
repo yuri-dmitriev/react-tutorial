92bd7e675da3fa99b8745ddfc93d796c569a0318
yuri-dmitriev <59753199+yuri-dmitriev@users.noreply.github.com>
2021-09-14 03:46:56 +0300

    Add basic prop types validation to Counter

--- src/04_props/Lesson.jsx
+++ src/04_props/Lesson.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const Counter = ({counter, func, number, string}) => {
   console.log(counter, func, number, string);
   return <h1>{`Counter component. Counter value is: ${counter}`}</h1>
+};
+
+Counter.propTypes = {
+  counter: PropTypes.number.isRequired,
+  func: PropTypes.func,
+  number: PropTypes.number,
+  string: PropTypes.string,
+};
+
+Counter.defaultProps = {
+  func: () => {},
+  number: 0,
+  string: '',
 };
 
 class CounterButton extends Component {
@@ -24,9 +38,6 @@ class CounterButton extends Component {
         <div>{counter}</div>
         <Counter
           counter={counter}
-          func={() => {}}
-          number={1}
-          string="str"
         />
         <button onClick={this.handleClick}>+1</button>
       </div>
