97a09899965dd0a253d1d279ea2401303343c830
yuri-dmitriev <59753199+yuri-dmitriev@users.noreply.github.com>
2021-09-16 21:46:01 +0300

    Create 3 tabs that can be switched by buttons

--- src/07_conditional_rendering/Lesson.jsx
+++ src/07_conditional_rendering/Lesson.jsx
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
-const ValidationMsg = ({val}) => {
-  if (val >= 10) {
-    return <h2>Greater than 10</h2>
-  } else {
-    return <h3>Less than <em>10</em></h3>
-  }
-};
+const Tab1 = () => (
+  <h1>Text of Tab1</h1>
+);
+
+const Tab2 = () => (
+  <h1>Text of Tab2</h1>
+);
+
+const Tab3 = () => (
+  <h1>Text of Tab3</h1>
+);
 
 class App extends Component {
+  state = {
+    activeTab: 1,
+  };
+
+  handleTab = (e) => {
+    this.setState({
+      activeTab: +e.target.getAttribute('data-name'),
+    });
+  };
 
   render() {
+    const { activeTab } = this.state;
     return (
-      <ValidationMsg val={8}/>
+      <Fragment>
+        <button data-name={1} onClick={this.handleTab}>Tab1</button>
+        <button data-name={2} onClick={this.handleTab}>Tab2</button>
+        <button data-name={3} onClick={this.handleTab}>Tab3</button>
+        {activeTab === 1 && <Tab1/>}
+        {activeTab === 2 && <Tab2/>}
+        {activeTab === 3 && <Tab3/>}
+        <div>
+          {`Active tab is: ${activeTab === 1 ? 'first' : activeTab === 2 ? 'second' : 'third'}`}
+        </div>
+      </Fragment>
     );
   }
 }
