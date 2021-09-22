49163c8ec347ece65a7c7224c164e1928120efdc
yuri-dmitriev <59753199+yuri-dmitriev@users.noreply.github.com>
2021-09-23 00:14:30 +0300

    Feat: create simple router for app

---
+++ src/18_router_1/components/posts/posts.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './posts.css';
+
+const Posts = () => (
+  <h1>Posts Page</h1>
+);
+
+export default Posts;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import News from './17_api_2/containers/news/news';
+import App from './18_router_1/Lesson';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(
-  <News />,
+import Home from './18_router_1/components/home/home';
+import Contacts from './18_router_1/components/contacts/contacts';
+import Posts from './18_router_1/components/posts/posts';
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/contacts" component={Contacts}/>
+        <Route path="/posts" component={Posts}/>
+      </Switch>
+    </App>
+  </BrowserRouter>
+  ),
   document.getElementById('root')
 );
