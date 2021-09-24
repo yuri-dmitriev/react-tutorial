f85b3e5ab62cadcd50c511efc9dfa1ae4625fa61
yuri-dmitriev <59753199+yuri-dmitriev@users.noreply.github.com>
2021-09-25 00:52:57 +0300

    Fix: console alert stateless HOC wrapping stateful Link

--- src/13_hoc/Lesson.jsx
+++ src/13_hoc/Lesson.jsx
@@ -1,11 +1,15 @@
 import { Component } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-const AppLink = (props) => ({
-  render: () => (
-    <Link {...props} activeClassName="active" />
-  )
-});
+const withActiveClassName = (Wrapped) => (
+  class extends Component {
+    render() {
+      return <Wrapped {...this.props} activeClassName="active">{this.props.children}</Wrapped>
+    }
+  }
+);
+
+const AppLink = withActiveClassName(Link);
 
 class Lesson extends Component {
   render() {
