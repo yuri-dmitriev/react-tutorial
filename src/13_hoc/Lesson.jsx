42fe017f2517ccd8cdfbf4d1eb41988c2baef5fe
yuri-dmitriev <59753199+yuri-dmitriev@users.noreply.github.com>
2021-09-25 03:29:02 +0300

    Feat: add preloader to AppComponent using loadingHOC

--- src/13_hoc/Lesson.jsx
+++ src/13_hoc/Lesson.jsx
@@ -1,4 +1,7 @@
 import { Component } from 'react';
+import LoadingHOC from './loadingHOC/loadingHOC';
+
+/*
 import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import './styles.css';
 
@@ -22,6 +25,41 @@ class Lesson extends Component {
           <AppLink to="/contacts">Contacts</AppLink>
         </nav>
       </Router>
+    );
+  }
+}
+*/
+
+class AppComponentUI extends Component {
+  render() {
+    return (
+      <div>{this.props.data.title}</div>
+    );
+  }
+}
+
+const AppComponent = LoadingHOC(AppComponentUI, 'data');
+
+class Lesson extends Component {
+  state = {
+    data: {},
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos/2')
+      .then(response => response.json())
+      .then(data => this.updateState(data));
+  }
+
+  updateState = (data) => {
+    setTimeout(() => {
+      this.setState({ data });
+    }, 3000);
+  };
+
+  render() {
+    return (
+      <AppComponent data={this.state.data} />
     );
   }
 }
